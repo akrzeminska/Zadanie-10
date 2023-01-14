a51c97dc5953a08fa2b8987aaba0910b3aea3c0c
akrzeminska <58666085+akrzeminska@users.noreply.github.com>
2023-01-14 19:27:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Zadanie-10
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-fc7xn6)
---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Napisz funkcję, która będzie łączyć napisy
+ *
+ * Założenia:
+ * - funkcja przyjmuje tablice napisów
+ * - funkcja zwraca typ string
+ * - Pomiędzy argumentami występuje whitespace
+ *
+ */
+
+function concatStrings() {}
+
+console.log(concatStrings(['A', 'B', 'C']) === 'A B C');
+
+console.log(concatStrings(['A', 'B', 'C', 'D', 'E']) === 'A B C D E');
